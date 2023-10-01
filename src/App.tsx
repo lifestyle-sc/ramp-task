9936ab1cc56499aad6ed6879e20cdcefee48c8f6
Oluwakayode Momoh <kayodemmh@gmail.com>
2023-10-01 09:10:54 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ data, setData ] = useState('');
+  const [ loading, setLoading ] = useState(false);
+  const spanRef: MutableRefObject<HTMLSpanElement | null> = useRef(null);
+  let index = 0;
+  const url = "https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/637261";
+
+  const fetchData = async () => {
+    setLoading(true);
+    const res = await fetch(url);
+    const data = await res.text();
+    setData(data);
+    setLoading(false);
+  };
+
+  const typeEffect = () => {
+    const delay = 500;
+
+    if(spanRef.current !== null){
+      const addedCharacter = data.substring(0, index + 1);
+      spanRef.current.innerText = `${addedCharacter}`;
+      index += 1;
+    }
+    if(index < data.length){
+      setTimeout(typeEffect, delay);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+    if(data && spanRef.current !== null){
+      typeEffect();
+    }
+  }, [data])
+
+  if(loading){
+    return <h2>Loading...</h2>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome To Ramp Task</h1>
+      <span ref={spanRef} className='txt'></span>
     </div>
   );
 }
